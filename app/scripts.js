284328b17cf98810ed545511ebcdc5f84b1c9106
Thomas Guillet <guillet.thomas@gmail.com>
2019-03-12 15:41:09 +0100

    Ajout de la directive à script.js

--- app/scripts.js
+++ app/scripts.js
@@ -96,6 +96,7 @@ require('./js/controllers/resultat.js');
 require('./js/controllers/stats.js');
 require('./js/controllers/validation.js');
 
+require('./js/directives/benefitCta.js');
 require('./js/directives/breadcrumb.js');
 require('./js/directives/captureContinuationRessource.js');
 require('./js/directives/captureMontantRessource.js');
