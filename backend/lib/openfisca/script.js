56a3b95b8ab091e27fdbdc11294e0e14e064d8f2
Thomas Guillet <guillet.thomas@gmail.com>
2019-05-27 11:35:06 +0200

    WIP

--- backend/lib/openfisca/script.js
+++ backend/lib/openfisca/script.js
@@ -20,17 +20,21 @@ var Situation = mongoose.model('Situation');
 const benefits = [
   'rsa',
   'ppa',
-  'aide_logement'
+  'aide_logement',
+  'aah'
 ];
 
 //const id = '5cdeaff798caa56428d468f3'; // SDS
-const id = '5ce5796705cba8696945b476'; // Locataire
+//const id = '5ce5796705cba8696945b476'; // Locataire
+//const id = '5ce80a33636cb968b6852cad'; // 2 adultes
+const id = '5ce80bac598c286abed27e32'; // seul en situation de handicap
 
 const variable = 'salaire_net';
 
 var values = [];
-var steps = 20 + 1;
-var max = 2000;
+var base = 30;
+var steps = base + 1;
+var max = base * 100;
 for (var i=0; i<steps; i = i+1) {
   values.push(i * max / (steps-1));
 }
@@ -39,17 +43,37 @@ const fullTimePeriod = 'month:2017-05:' + fullTimePeriodLength.toString();
 
 function extractResults({ source, response }) {
   const periods = common.getPeriods(source.dateDeValeur);
-  const keys = Object.keys(response.familles);
+  const entities = ['familles', 'individus'];
 
-  return keys.reduce((a, k) => {
-    var b = benefits.reduce((a, v) => {
-      a[v] = response.familles[k][v][periods.thisMonth];
-      return a;
-    }, {});
+  return entities.reduce((groupAccum, group) => {
+    const entityNames = Object.keys(response[group]);
+    return entityNames.reduce((entityAccum, id) => {
+      const prefix = id.split('_')[0];
+      entityAccum[prefix] = entityAccum[prefix] || {};
 
-    a[k] = b;
-    return a;
-  }, {});
+      return benefits.reduce((benefitAccum, variable) => {
+        const base = response[group][id][variable];
+        if (base) {
+          benefitAccum[prefix][variable] = base[periods.thisMonth];
+        }
+
+        return benefitAccum;
+      }, entityAccum);
+    }, groupAccum);
+  }, {})
+}
+
+function dumpResults(e) {
+
+  var ks = Object.keys(e);
+  var r = ks.map(k => {
+    var n = k.split('_')[0];
+    return [n, n, e[k].rsa, e[k].ppa, e[k].aide_logement, e[k].aah].join(';');
+  }).join('\n').replace(/\./g,',');
+
+  var h = ['', 'SN', 'RSA', 'PPA', 'AL', 'AAH'].join(';');
+  console.log(h);
+  console.log(r);
 }
 
 function main() {
@@ -86,6 +110,7 @@ function main() {
     .then(s => {
       const results = extractResults(s)
       console.log(results)
+      dumpResults(results);
       return s;
     })
     .catch(e => {
